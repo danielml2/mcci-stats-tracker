3219aa29091b58b50fba5cbff3da4701b1b29354
Daniel <64473184+danielml2@users.noreply.github.com>
2023-07-09 17:03:33 +0300

    Removed/edited example mod setup
    - Removed example mixin
    - Edited the fabric mod config

--- src/main/java/net/fabricmc/example/MCCIStats.java
+++ src/main/java/net/fabricmc/example/MCCIStats.java
@@ -4,11 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class MCCIStats implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("mcci-stats-tracker");
 
 	@Override
 	public void onInitialize() {
